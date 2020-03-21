3aa613ed5da49968a218ff9b006dd5ad376ce893
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-03-21 15:08:57 +0530

    Fix Docs [ci skip]

--- pages/src/docs/swBase.js
+++ pages/src/docs/swBase.js
@@ -1,10 +1,10 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.1/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({ prefix: 'Zusty' });
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-workbox.precaching.precacheAndRoute([], {
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
   cleanUrls: true
 });
 
